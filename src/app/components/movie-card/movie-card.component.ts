389e406b82a2256ca150302b8ba329bed1d4de33
justin <JustinCarpenter2020@Gmail.com>
2022-02-09 12:51:31 -0700

    limiting characters

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -7,10 +7,17 @@ import { movie } from 'src/app/models/movie';
   styleUrls: ['./movie-card.component.css']
 })
 export class MovieCardComponent implements OnInit {
-@Input() movie : movie | undefined
+@Input() movie : movie = {
+  title: '',
+  score: 0,
+  imgUrl: '',
+  description: ''
+}
+briefOverview: string = ''
   constructor() { }
 
   ngOnInit(): void {
+    this.briefOverview = `${this.movie.description.slice(0, 80)}...`
   }
 
 }
