0b673f343ebad43e02d975ade615b9189915b689
justin <JustinCarpenter2020@Gmail.com>
2022-02-09 11:57:48 -0700

    cards display movie data

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'movie-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-card.component.css']
 })
 export class MovieCardComponent implements OnInit {
-
+@Input() movie : movie | undefined
   constructor() { }
 
   ngOnInit(): void {
